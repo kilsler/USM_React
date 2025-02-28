5271ae93beef151a37e3efb6f56dbe2ae0234e89
kilsler <126207975+kilsler@users.noreply.github.com>
2025-02-28 12:23:33 +0200

    Delete 02-style-ways/03_css_modules directory

--- 02-style-ways/03_css_modules/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-    return (
-      <footer>
-        © {new Date().getFullYear()} Название компании. Все права защищены.
-      </footer>
-    );
-   }
-
-export default Footer;
