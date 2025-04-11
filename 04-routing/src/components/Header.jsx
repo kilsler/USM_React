1769ef9c2438a373af5fa666b26ef12dd0068eda
kilsler <126207975+kilsler@users.noreply.github.com>
2025-04-11 06:57:10 +0000

    Update Header.jsx

--- 04-routing/src/components/Header.jsx
+++ 04-routing/src/components/Header.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router"
 function Header() {
   return (
     <>
-     <nav className="bg-gray-800 w-full h-16 text-sky-200 text-xl">
+     <nav className="bg-gray-800 w-full h-16 text-sky-200 text-xl mb-4">
         <img src="/src/assets/react.svg" alt="logo" className="absolute left-4 top-2 w-12 h-12 rounded-full ml-8" />
         <div className="flex items-center justify-end-safe gap-14 h-full px-4 mr-30">
           <NavLink to="/" className="text-sky-200 hover:text-sky-400 font-mono font-medium h-full pt-5">Home</NavLink>
