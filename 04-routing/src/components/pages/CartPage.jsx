e9537daef7209780ea53c486aeea46eaf210235c
kilsler <126207975+kilsler@users.noreply.github.com>
2025-04-11 07:53:13 +0000

    Update CartPage.jsx

--- 04-routing/src/components/pages/CartPage.jsx
+++ 04-routing/src/components/pages/CartPage.jsx
@@ -1,8 +1,14 @@
 export default function CartPage(){
     return (
-        <>
-        <h2>The cart page is still under construction...</h2>
-        </>
+    <section className="bg-gray-900">
+            <div className="grid max-w-screen-xl px-4 mx-auto gap-8 py-16 grid-cols-12">
+                <div className="mr-auto place-self-center lg:col-span-7">
+                    <h1 className="max-w-2xl mb-4 text-6xl font-extrabold tracking-tight leading-none text-white">
+                        Cart page under construction!
+                    </h1>
+                </div>                
+            </div>
+        </section>
     )
 
 }
